1f078f8697af02e704463ac04f89e7b5c8f93412
Edward Geng <42809151+Eddie2shoes@users.noreply.github.com>
2022-06-06 20:49:03 -0400

    fixed typo

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowswerRouter as Router, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider>
